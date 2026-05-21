6eefbb2a352c5d8247e1c631346d3372db076983
Guilherme Arruda <g174618@dac.unicamp.br>
2026-05-21 09:37:14 -0300

    Criação do algoritmo espaçado e refatoração da classe de flashcards

--- app/src/main/java/modelo/Flashcard.java
+++ app/src/main/java/modelo/Flashcard.java
@@ -1,15 +1,25 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Flashcard extends MaterialDeEstudo implements Revisar {
     private String frente;
     private String verso;
     private int facilidade;
+    private int repeticoes;
+    private int intervalos;
+    private LocalData DataProximaRevisao;
     
     public Flashcard(String titulo, String disciplina, String frente, String verso){
         super(titulo, disciplina);
         this.frente = frente;
         this.verso = verso;
         this.facilidade = 100;
+
+        this.repeticoes = 0;
+        this.facilidade = 2.5;
+        this.intervalos = 0;
+        this.DataProximaRevisao = LocalDate.now();
     }
     
     @Override
@@ -30,8 +40,28 @@ public class Flashcard extends MaterialDeEstudo implements Revisar {
     public void setVerso(String novo_verso){
         this.verso = novo_verso;
     }
-    public void calcularProximaRevisao(int desempenho){
-        this.facilidade += (desempenho * 100);
-        System.out.println("Troca equivalente realizado com sucesso! Conhecimento transmutado. Nível de facilidade: " + this.facilidade);
+    public String getRepeticoes(){
+        return repeticoes;
+    }
+    public void setRepeticoes(int repeticoes){
+        this.repeticoes = repeticoes;
+    }
+    public double getFacilidade(){
+        return facilidade;
+    }
+    public void setFacilidade(int facilidade){
+        this.facilidade = facilidade;
+    }
+    public int getIntervalosDias(){
+        return intervalos;
+    }
+    public void setIntervalos(int intervalos){
+        this.intervalos = intervalos;
+    }
+    public LocalDate getDataProximaRevisao(){
+        return DataProximaRevisao;
+    }
+    public void setDataProximaRevisao(LocalDate dataProximaRevisao){
+        this.DataProximaRevisao = dataProximaRevisao;
     }
 }
