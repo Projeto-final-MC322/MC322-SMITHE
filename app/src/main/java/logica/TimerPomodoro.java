4331891b9e3177718322fb9da508c6a7ba2f6f33
Guilherme Arruda <g174618@dac.unicamp.br>
2026-05-23 21:35:06 -0300

    Biblioteca Gson do google adicionada

---
+++ app/src/main/java/logica/TimerPomodoro.java
@@ -0,0 +1,7 @@
+package logica;
+public class TimerPomodoro {
+    private int tempoFoco = 25;
+    private int tempoPausa = 5;
+    private int ciclos = 0;
+    private boolean 
+}
--- app/src/main/java/save/JSON.java
+++ app/src/main/java/save/JSON.java
@@ -2,35 +2,47 @@ package save;
 import main.java.*;
 import modelo.GerenciadorDeConteudo;
 
+import modelo.Flashcard;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class JSON implements Armazenamento {
+    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+
     @Override
     public void salvarDados(GerenciadorDeConteudo gerenciador, String path){
-        System.out.println("Iniciando...")
+        System.out.println("Iniciando...");
 
         try(FileWriter writter = new FileWriter(path)){
-            writter.write("{\n");
-            writter.write(" \"projeto\": \"SMITHE\",\n");
-            writter.write(" \"status\": \"Dados salvos localmente\"\n");
-            writter.write("}\n");
+            List<Flashcard> todososCartoes = gerenciador.obtercards_hoje();
+
+            gson.toJson(todososCartoes, writter);
 
             System.out.println("Oh yeah! Ficheiro salvo com sucesso em: " + path);
         } catch (IOException e) {
             System.out.println("Falha ao gravar dados no disco: " + e.getMessage());
         }
     }
+
     @Override
     public void carregarDados(GerenciadorDeConteudo gerenciador, String path){
+        try(FileReader reader = new FileReader(path)) {
+            Type tipo = new TypeToken<List<Flashcard>>(){}.getType();
+            List<Flashcard> cartoesRecuperados = gson.fromJson(reader, tipo);
 
-        try(BufferReader reader = new BufferedReader(new FileReader(path))) {
-            String row;
-            System.out.println("Lendo estrutura do arquivo JSON:");
-            while((row = leitor.readLine()) != null){
-                System.out.println(" " + linha);
+            if(cartoesRecuperados != null){
+                for(Flashcard card : cartoesRecuperados){
+                    gerenciador.adicionarCartao(card);
+                }
             }
             System.out.println("Banco de dados mental restaurado com sucesso!");
         }catch (IOException e){
