75957e201d166d41c7efd297dc17f5952212936f
Victória Barreiro Leite <v285636@dac.unicamp.br>
2026-05-31 16:53:06 -0300

    atualizações deque

--- app/src/main/java/modelo/Deque.java
+++ app/src/main/java/modelo/Deque.java
@@ -2,11 +2,23 @@ package modelo;
 import java.util.ArrayList;
 
 public class Deque {
-    private ArrayList<Flashcard> flashcards;
+    
+    private String materia;
+    private ArrayList<Flashcard> flashcards; // flashcards por materia
+    // private ArrayList<MapaMental> mapas; // mapas mentais por materia
 
-    // inicializa o deque de flashcards
-    public void setDeque(){
-        flashcards = new ArrayList<Flashcard>();
+    public Deque(String materia){
+        this.materia = materia;
+        this.flashcards = new ArrayList<>();
+        // this.mapas = new ArrayList<>();
+    }
+
+    public String getMateria(){
+        return this.materia;
+    }
+
+    public void setMateria(String materia){
+        this.materia = materia;
     }
 
     // aidiciona no final do deque
@@ -14,15 +26,27 @@ public class Deque {
         this.flashcards.add(flashcard);
     }
 
+    // public void addMap(MapaMental mapa){
+    //     this.mapas.add(mapa);
+    // }
+
     // retorna o flashcard de índice i
     public Flashcard getFlashcard(int i){
         return this.flashcards.get(i);
     }
 
+    // public MapaMental getMap(int i){
+    //     return this.mapas.get(i);
+    // }
+
     // remove por inidice
     public void removeFlashcard(int i){
         this.flashcards.remove(i);
     }
+
+    // public void removeMap(int i){
+    //     this.mapas.remove(i);
+    // }
 
     // // ordena o deque de flashcards por facilidade
     // public void sortFlashcards(){
